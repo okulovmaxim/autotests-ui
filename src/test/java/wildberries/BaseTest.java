d06707d6bf55c7b82e61a7fbd7adfb509589ed68
okulovmaxim <okulov.m.i@gmail.com>
2024-12-03 21:57:16 +0500

    Update

--- src/test/java/wildberries/BaseTest.java
+++ src/test/java/wildberries/BaseTest.java
@@ -11,7 +11,7 @@ import java.time.Duration;
 
 public class BaseTest {
 
-    private WebDriver driver;
+    protected WebDriver driver;
 
     @BeforeSuite
     public static void downloadDriver() {
---
+++ src/test/java/wildberries/WbTests.java
@@ -0,0 +1,12 @@
+package wildberries;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class WbTests extends BaseTest {
+
+    @Test
+    public void searchResultTest() {
+        driver.findElement(By.xpath("ssss"));
+    }
+}
